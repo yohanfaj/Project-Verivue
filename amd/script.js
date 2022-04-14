d8436b47ad58dea2e18d98182c7b0ed933c8a0da
Glitch (project-verivue) <none>
2022-04-14 07:00:59 +0000

    First step of the AMD Test implemented

--- amd/script.js
+++ amd/script.js
@@ -6,9 +6,8 @@ const negative_answer_btn = document.querySelector("#no_answer");
 // Detect clicks on the button
 if (negative_answer_btn) {
   negative_answer_btn.onclick = function () {
-    score_amd++;
+    amdImgNumber++;
     isAMD = true;
-    console.log(score_amd);
   };
 }
 
@@ -18,6 +17,13 @@ console.log(positive_answer_btn)
 if (positive_answer_btn) {
   positive_answer_btn.onclick = function() {
     window.alert("clicked");
+    amdImgNumber++;
+    document.getElementById("amsler_test").src="https://cdn.glitch.global/c0e804fa-f500-46aa-88e7-c70999b4319c/amsler_test2.jpg?v=1649917780188";
+  }
+}
+
+function changeImage(){
+  if (amdImgNumber == 1){
     document.getElementById("amsler_test").src="https://cdn.glitch.global/c0e804fa-f500-46aa-88e7-c70999b4319c/amsler_test2.jpg?v=1649917780188";
   }
 }
