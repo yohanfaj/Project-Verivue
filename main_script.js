1fc5ce4707602021fb74da01c0f0cbe33f25f407
Glitch (verivue) <none>
2022-05-17 09:35:14 +0000

    [LOUIS] Cookies export works + new home page + Do All Tests activated

--- main_script.js
+++ main_script.js
@@ -1,14 +1,28 @@
+//Start up instructions:
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+window.onload = function() {
+ setCookie("doAllTests", "false",1);
+};
+
 var btn_audio_check = new Audio('https://cdn.glitch.global/c0e804fa-f500-46aa-88e7-c70999b4319c/check_button_audio.mp3?v=1649923001726');
 btn_audio_check.volume = 0.5;
 
 // STARTING BUTTONS
-
 const myopia_start_btn = document.querySelector("#myopia_start"); // Get the button from the page
 // Detect clicks on the button
 if (myopia_start_btn) {
   myopia_start_btn.onclick = function () {
     btn_audio_check.play();
-    window.open("myopia/index.html", "_self");
+    window.open("myopia", "_self");
   };
 }
 
@@ -17,7 +31,7 @@ const amd_start_btn = document.querySelector("#amd_start"); // Get the button fr
 if (amd_start_btn) {
   amd_start_btn.onclick = function () {
     btn_audio_check.play();
-    window.open("amd/index.html", "_self");
+    window.open("amd/", "_self");
   };
 }
 
@@ -26,7 +40,7 @@ const astig_start_btn = document.querySelector("#astig_start"); // Get the butto
 if (astig_start_btn) {
   astig_start_btn.onclick = function () {
     btn_audio_check.play();
-    window.open("astigmatism/index.html", "_self");
+    window.open("astigmatism/", "_self");
   };
 }
 
@@ -35,6 +49,16 @@ const dalton_start_btn = document.querySelector("#dalton_start"); // Get the but
 if (dalton_start_btn) {
   dalton_start_btn.onclick = function () {
     btn_audio_check.play();
-    window.open("daltonism/index.html", "_self");
+    window.open("daltonism/", "_self");
+  };
+}
+
+const doAllTest = document.querySelector("#doAllTest"); // Get the button from the page
+// Detect clicks on the button
+if (doAllTest) {
+  doAllTest.onclick = function () {
+    setCookie("doAllTests","true",1);
+    btn_audio_check.play();
+    window.open("amd/", "_self");
   };
 }
